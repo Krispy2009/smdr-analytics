31c03c1b37522869acd9b7905ccfc1503bba4cb7
Kristi Achilleos <krispy2009@gmail.com>
2019-07-14 12:34:34 +0300

    download attachments to folder

--- process.py
+++ process.py
@@ -1,52 +1,80 @@
+import os
+import sys
 import logme
+import email
 
 from imapclient import IMAPClient, exceptions
-import settings
+from settings import EMAIL_SERVER, EMAIL_USER, EMAIL_PASS, FOLDER_TO_SCAN
 
 SERVER = None
+
 
 @logme.log
 def setup(logger=None):
     global SERVER
-    logger.info(f"Connecting to {settings.EMAIL_SERVER}")
-    SERVER = IMAPClient(settings.EMAIL_SERVER)
-    
-    logger.info(f"Logging in: {settings.EMAIL_USER}")
-    SERVER.login(settings.EMAIL_USER, settings.EMAIL_PASS)
+
+    if not all([EMAIL_USER, EMAIL_PASS, EMAIL_SERVER]):
+        logger.error("Please provide all of the login information: email, password and host")
+        sys.exit(1)
+
+    logger.info(f"Connecting to {EMAIL_SERVER}")
+    SERVER = IMAPClient(EMAIL_SERVER)
+
+    logger.info(f"Logging in: {EMAIL_USER}")
+    SERVER.login(EMAIL_USER, EMAIL_PASS)
+
 
 @logme.log
 def get_attachments(logger=None):
-    try: 
-        logger.debug(f'Trying to get attachments from {settings.FOLDER_TO_SCAN}')
-        select_info = SERVER.select_folder(settings.FOLDER_TO_SCAN)
+    try:
+        logger.debug(f"Trying to get attachments from {FOLDER_TO_SCAN}")
+        select_info = SERVER.select_folder(FOLDER_TO_SCAN)
         logger.debug(f"Total emails: {select_info.get(b'EXISTS')}")
-        messages = SERVER.search('ALL')
+        messages = SERVER.search("ALL")
 
-        for uid, data in SERVER.fetch(messages, 'RFC822').items():
-            logger.warning(data)
+        for uid, data in SERVER.fetch(messages, "RFC822").items():
+            email_message = email.message_from_bytes(data[b"RFC822"])
+            for part in email_message.walk():
+                attachment_filename = part.get_filename()
+                if attachment_filename is not None:
+                    logger.warning(f"Downloading: {uid} - {attachment_filename}")
+                    if not os.path.exists("attachments"):
+                        os.mkdir("attachments")
+                        logger.info("created attachments directory")
+                    path = os.path.join(f"./attachments/{uid}_{attachment_filename}")
+                    if not os.path.isfile(path):
+                        with open(path, "wb") as f:
+                            f.write(part.get_payload(decode=True))
+                        logger.info(f"Wrote file: {path}")
 
     except exceptions.IMAPClientError as e:
         logger.error(e)
 
+
 def teardown(logger=None):
     resp = SERVER.logout()
     logger.info(resp)
-    logger.info('Logged out')
+    logger.info("Logged out")
 
 
 def read_attachments():
     pass
 
+
 def parse_smdr_data():
     pass
+
 
 def save_to_db(parsed_smdr_line):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    try:
+        setup()
+        get_attachments()
+    except:
+        logme.critical("OH NO")
+    finally:
 
-    setup()
-
-    get_attachments()    
-
+        teardown()
--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
-EMAIL_USER = os.getenv('EMAIL')
-EMAIL_PASS=os.getenv('EMAIL_PASSWORD')
-EMAIL_SERVER=os.getenv('HOST','outlook.office365.com')
 
-FOLDER_TO_SCAN=os.getenv('EMAIL_FOLDER','INBOX/ACMA-SMDR')
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
+EMAIL_SERVER = os.getenv("EMAIL_SERVER", "outlook.office365.com")
+FOLDER_TO_SCAN = os.getenv("EMAIL_FOLDER", "INBOX")
